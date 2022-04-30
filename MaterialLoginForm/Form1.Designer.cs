c51036274bf87bd595793ab7068f21221de769a6
Ardiansyah <aardi25@gmail.com>
2022-04-30 08:46:26 +0700

    Add project files.

---
+++ MaterialLoginForm/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace MaterialLoginForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lollipopLabel1 = new LollipopLabel();
+            this.lollipopTextBox1 = new LollipopTextBox();
+            this.lollipopLabel2 = new LollipopLabel();
+            this.lollipopTextBox2 = new LollipopTextBox();
+            this.lollipopCheckBox1 = new LollipopCheckBox();
+            this.lollipopButton1 = new LollipopButton();
+            this.lollipopLabel3 = new LollipopLabel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lollipopLabel1
+            // 
+            this.lollipopLabel1.AutoSize = true;
+            this.lollipopLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.lollipopLabel1.Font = new System.Drawing.Font("Roboto Medium", 10F);
+            this.lollipopLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(153)))), ((int)(((byte)(153)))));
+            this.lollipopLabel1.Location = new System.Drawing.Point(18, 202);
+            this.lollipopLabel1.Name = "lollipopLabel1";
+            this.lollipopLabel1.Size = new System.Drawing.Size(73, 18);
+            this.lollipopLabel1.TabIndex = 0;
+            this.lollipopLabel1.Text = "Username";
+            this.lollipopLabel1.Click += new System.EventHandler(this.lollipopLabel1_Click);
+            // 
+            // lollipopTextBox1
+            // 
+            this.lollipopTextBox1.FocusedColor = "#508ef5";
+            this.lollipopTextBox1.FontColor = "#999999";
+            this.lollipopTextBox1.IsEnabled = true;
+            this.lollipopTextBox1.Location = new System.Drawing.Point(21, 223);
+            this.lollipopTextBox1.MaxLength = 32767;
+            this.lollipopTextBox1.Multiline = false;
+            this.lollipopTextBox1.Name = "lollipopTextBox1";
+            this.lollipopTextBox1.ReadOnly = false;
+            this.lollipopTextBox1.Size = new System.Drawing.Size(238, 24);
+            this.lollipopTextBox1.TabIndex = 0;
+            this.lollipopTextBox1.TextAlignment = System.Windows.Forms.HorizontalAlignment.Left;
+            this.lollipopTextBox1.UseSystemPasswordChar = false;
+            // 
+            // lollipopLabel2
+            // 
+            this.lollipopLabel2.AutoSize = true;
+            this.lollipopLabel2.BackColor = System.Drawing.Color.Transparent;
+            this.lollipopLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lollipopLabel2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(153)))), ((int)(((byte)(153)))));
+            this.lollipopLabel2.Location = new System.Drawing.Point(19, 261);
+            this.lollipopLabel2.Name = "lollipopLabel2";
+            this.lollipopLabel2.Size = new System.Drawing.Size(69, 17);
+            this.lollipopLabel2.TabIndex = 0;
+            this.lollipopLabel2.Text = "Password";
+            this.lollipopLabel2.Click += new System.EventHandler(this.lollipopLabel1_Click);
+            // 
+            // lollipopTextBox2
+            // 
+            this.lollipopTextBox2.FocusedColor = "#508ef5";
+            this.lollipopTextBox2.FontColor = "#999999";
+            this.lollipopTextBox2.IsEnabled = true;
+            this.lollipopTextBox2.Location = new System.Drawing.Point(22, 284);
+            this.lollipopTextBox2.MaxLength = 32767;
+            this.lollipopTextBox2.Multiline = false;
+            this.lollipopTextBox2.Name = "lollipopTextBox2";
+            this.lollipopTextBox2.ReadOnly = false;
+            this.lollipopTextBox2.Size = new System.Drawing.Size(238, 24);
+            this.lollipopTextBox2.TabIndex = 1;
+            this.lollipopTextBox2.TextAlignment = System.Windows.Forms.HorizontalAlignment.Left;
+            this.lollipopTextBox2.UseSystemPasswordChar = false;
+            this.lollipopTextBox2.TextChanged += new System.EventHandler(this.lollipopTextBox2_TextChanged);
+            // 
+            // lollipopCheckBox1
+            // 
+            this.lollipopCheckBox1.AutoSize = true;
+            this.lollipopCheckBox1.CheckColor = "#508ef5";
+            this.lollipopCheckBox1.Location = new System.Drawing.Point(21, 325);
+            this.lollipopCheckBox1.Name = "lollipopCheckBox1";
+            this.lollipopCheckBox1.Size = new System.Drawing.Size(114, 20);
+            this.lollipopCheckBox1.TabIndex = 2;
+            this.lollipopCheckBox1.Text = "Remember me";
+            this.lollipopCheckBox1.UseVisualStyleBackColor = true;
+            // 
+            // lollipopButton1
+            // 
+            this.lollipopButton1.BackColor = System.Drawing.Color.Transparent;
+            this.lollipopButton1.BGColor = "#508ef5";
+            this.lollipopButton1.FontColor = "#ffffff";
+            this.lollipopButton1.Location = new System.Drawing.Point(20, 366);
+            this.lollipopButton1.Name = "lollipopButton1";
+            this.lollipopButton1.Size = new System.Drawing.Size(238, 41);
+            this.lollipopButton1.TabIndex = 3;
+            this.lollipopButton1.Text = "Login";
+            // 
+            // lollipopLabel3
+            // 
+            this.lollipopLabel3.AutoSize = true;
+            this.lollipopLabel3.BackColor = System.Drawing.Color.Transparent;
+            this.lollipopLabel3.Font = new System.Drawing.Font("Roboto Medium", 10F);
+            this.lollipopLabel3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(153)))), ((int)(((byte)(153)))));
+            this.lollipopLabel3.Location = new System.Drawing.Point(59, 163);
+            this.lollipopLabel3.Name = "lollipopLabel3";
+            this.lollipopLabel3.Size = new System.Drawing.Size(148, 18);
+            this.lollipopLabel3.TabIndex = 4;
+            this.lollipopLabel3.Text = "Login to Your Account";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::MaterialLoginForm.Properties.Resources._default;
+            this.pictureBox1.Location = new System.Drawing.Point(80, 72);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 77);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(274, 438);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lollipopLabel3);
+            this.Controls.Add(this.lollipopButton1);
+            this.Controls.Add(this.lollipopCheckBox1);
+            this.Controls.Add(this.lollipopTextBox2);
+            this.Controls.Add(this.lollipopLabel2);
+            this.Controls.Add(this.lollipopTextBox1);
+            this.Controls.Add(this.lollipopLabel1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private LollipopLabel lollipopLabel1;
+        private LollipopTextBox lollipopTextBox1;
+        private LollipopLabel lollipopLabel2;
+        private LollipopTextBox lollipopTextBox2;
+        private LollipopCheckBox lollipopCheckBox1;
+        private LollipopButton lollipopButton1;
+        private LollipopLabel lollipopLabel3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ MaterialLoginForm/Form1.cs
@@ -0,0 +1,26 @@
+﻿using MaterialSkin.Controls;
+
+namespace MaterialLoginForm
+{
+    public partial class Form1 : MaterialForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            MaterialSkin.MaterialSkinManager manager = MaterialSkin.MaterialSkinManager.Instance;
+            manager.AddFormToManage(this);
+            manager.Theme = MaterialSkin.MaterialSkinManager.Themes.LIGHT;
+            manager.ColorScheme = new MaterialSkin.ColorScheme(MaterialSkin.Primary.Blue300, MaterialSkin.Primary.Blue500, MaterialSkin.Primary.Blue500, MaterialSkin.Accent.LightBlue400, MaterialSkin.TextShade.WHITE);
+        }
+
+        private void lollipopLabel1_Click(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void lollipopTextBox2_TextChanged(object sender, System.EventArgs e)
+        {
+
+        }
+    }
+}
